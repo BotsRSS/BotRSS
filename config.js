02fd26cdeb67c24e620748704b3c4233b90b910c
muit <miguel_3c@hotmail.com>
2016-03-26 22:39:39 +0100

    Ignore config, OnFollow event, oauth2 check

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
   twitter: {
-    consumer_key: '',
-    consumer_secret: '',
-    access_token: '',
-    access_token_secret: ''
+    consumer_key: '2mu6RYExjaTLE9CM4saQLAP4C',
+    consumer_secret: 'yZxcq9TnWGhX2nANjQMRVP0lbaNA775ONaqn2PUF1NTdx5PcOM',
+    access_token: '713772632475377664-8gR6tf13KFf9QVzEtD5KkSvDp6JnkOM',
+    access_token_secret: 'SK1oaGJr4lNACr5O83MPI5yZwwDYylN3kopetLQmE6HuI'
   },
   
   autoRetweet: false,
